49e98f7c4d947f66b746d2f2295be2306f9f337b
AbdulAhadMunaf <abdulahadmunaf4@gmail.com>
2024-06-16 17:35:59 +0500

    Created my first api route and used postman for testing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 // require('dotenv').config({path: './env'});
 
 import dotenv from "dotenv";
-import express from "express"
-const app = express();
-
+import app from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
         path: "/.env"
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 connectDB()
         .then(() => {
                 app.on("error", (error) => {
                         console.log("ERR:", error);
                         throw error;
                 })
-                app.listen(port || 8000, () => {
+                app.listen(port || 3000, () => {
                         console.log(`Server is running at port: ${port}`);
                 })
         })
